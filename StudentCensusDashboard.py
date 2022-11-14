e96615b6310b0c54561b972012bf274b54f7e988
cdholmes11 <coreydaneholmes@gmail.com>
2022-11-14 12:25:30 -0600

    Update StudentCensusDashboard.py

--- StudentCensusDashboard.py
+++ StudentCensusDashboard.py
@@ -21,12 +21,13 @@ st.set_page_config(
 def init_connection():
     return connection.connect(**st.secrets["mysql"])
 
+mydb = init_connection()
+
 # Perform query.
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
 @st.experimental_memo(ttl=60000)
 def load_data():
     try:
-        mydb = init_connection()
         query = """
             select
                 *
